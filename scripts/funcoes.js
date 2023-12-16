9b98787ea0c9251ec8546acdb156388a85780c76
vdaschagas <valterdaschagas@gmail.com>
2023-12-16 19:57:04 -0300

    Add files via upload

---
+++ scripts/funcoes.js
@@ -0,0 +1,12 @@
+var quebrada = false;
+function Lampada(){
+    if(tipo == 1){
+        arquivo = "../img/image.jpg"
+    }
+    if(!quebrada){
+        document.getElementById("item-contato").id("menu-item");
+        if(tipo == 3){
+            quebrada = true;
+        }
+    }
+}
