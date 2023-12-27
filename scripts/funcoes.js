a6b64c56a8886155a7e525236245993e35ce3ec6
vdaschagas <valterdaschagas@gmail.com>
2023-12-27 14:41:31 -0300

    Layout
    Atualização para novo layout

--- scripts/funcoes.js
+++ scripts/funcoes.js
@@ -1,4 +1,3 @@
-/* == INICIO ==*/
 
 /* ANIMAR */
 function animar() {
@@ -25,5 +24,25 @@ function clickMenu() {
         itens.style.display = 'block'
     }
 }
+
+window.addEventListener("scroll", function(){
+    let header = document.querySelector('.cabecalho')
+    header.classList.toggle('rolagem', window.scrollY > 0)
+});
+    
+
+var onda1= documento.getElementById('onda1')
+var onda2= documento.getElementById('onda2')
+var onda3= documento.getElementById('onda3')
+var onda4= documento.getElementById('onda4')
+
+window.addEventListener('scrool', function(){
+    var rolagemPos = window.scrollY
+
+    onda1.style.backgroundPositionX = 400 + rolagemPos * 8 + 'px'
+    onda2.style.backgroundPositionX = 300 + rolagemPos * -6 + 'px'
+    onda3.style.backgroundPositionX = 200 + rolagemPos * 4 + 'px'
+    onda4.style.backgroundPositionX = 100 + rolagemPos * -2 + 'px'
+})
 
 
